f54a275dec7af14250dd06067c236f536a419333
ruchiragoswami <ruchigoswami8266@gmail.com>
2022-02-03 17:26:14 -0800

    create js file

---
+++ 1-documentation/doc-avatar/doc-avatar.js
@@ -0,0 +1,74 @@
+
+    const menuBtn = document.querySelector("#menu-btn");
+    const thinzyNav = document.querySelector(".thinzyNav");
+    const menuList = document.querySelector(".menuList");
+
+
+    menuBtn.addEventListener('click', toggleMenu);
+
+
+
+    // function toggleMenu()
+    //     thinzyNav.classList.add('open-menu');
+    //     thinzyNav.classList.toggle('close-menu'); 
+    // }
+
+    // thinzyNav.style.display = "none";
+
+    // if (window.screen.width >= '830px') {
+    //     thinzyNav.style.display = "block";
+    // } else {
+    //    thinzyNav.style.display = "none" ;
+
+    // }
+
+
+    //     function toggleMenu(){
+    //         if (thinzyNav.style.display == "none") {
+    //                 thinzyNav.style.display = "block";                
+    //             } else if (thinzyNav.style.display == "block" ) {
+    //                 thinzyNav.style.style = 'none'; 
+    //             } else {
+    //                 console.log('you came here');
+    //             }
+    //     }
+
+
+    // menuBtn.addEventListener('click', toggleMenu);
+
+
+    // if (window.screen.width >= '830px') {
+    //         thinzyNav.style.display = "block";
+    //     } else {
+    //         thinzyNav.style.display = "none";
+    //     }
+
+    // thinzyNav.style.display = 'block'; 
+
+    // if (thinzyNav.style.display = "none") {
+    //     menuBtn.style.display ="block";
+    //     menuBtn.addEventListener('click', menuToggle); 
+
+
+    //    
+
+
+
+
+
+
+
+    // function menuToggle() {
+    //         console.log('you did click')
+    //         if (thinzyNav.style.display === "block") {
+    //             thinzyNav.style.display = "block";
+    //             console.log(success); 
+    //         } else if (thinzyNav.style.display === "none") {
+    //             thinzyNav.style.display = "none";
+    //         } else if (window.screen.width >= '830px' ) {
+    //     thinzyNav.style.display = "block";
+    // }
+    //     }
+
+    //     if (window.screen.width >= '830px') {
+    //     bigScreen = true;  
